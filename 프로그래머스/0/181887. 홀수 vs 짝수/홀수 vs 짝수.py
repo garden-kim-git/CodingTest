c97f34cf0152a3ee079a940ec36250cee4156b20
김정원 <53960185+garden-kim-git@users.noreply.github.com>
2026-04-22 14:53:37 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    answer = 0
+    sum1, sum2 = 0, 0
+    for i in range(len(num_list)) :
+        if i % 2 == 0 : sum1 += num_list[i]
+        else : sum2 += num_list[i]
+    answer = max(sum1, sum2)
+    return answer
