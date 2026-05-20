a423d6405382849ce9ae6e0e98480b6c2d78b822
김정원 <53960185+garden-kim-git@users.noreply.github.com>
2026-05-20 09:19:16 +0900

    [level 0] Title: 세 개의 구분자, Time: 15.20 ms, Memory: 22.5 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,13 +1,20 @@
+# def solution(myStr):
+#     answer = []
+#     temp = ""
+    
+#     for k in myStr :
+#         if k == 'a' or k == 'b' or k == 'c' :
+#             if len(temp) != 0 : answer.append(temp)
+#             temp = ""
+#         else :
+#             temp += k
+#     if len(temp) != 0 : answer.append(temp)
+#     if len(answer) == 0 : answer.append("EMPTY")
+#     return answer
+
 def solution(myStr):
     answer = []
-    temp = ""
+    answer = " ".join(myStr.replace("a", " ").replace("b", " ").replace("c", " ").split()).split()
+    if answer==[] : answer = ["EMPTY"]
     
-    for k in myStr :
-        if k == 'a' or k == 'b' or k == 'c' :
-            if len(temp) != 0 : answer.append(temp)
-            temp = ""
-        else :
-            temp += k
-    if len(temp) != 0 : answer.append(temp)
-    if len(answer) == 0 : answer.append("EMPTY")
     return answer
