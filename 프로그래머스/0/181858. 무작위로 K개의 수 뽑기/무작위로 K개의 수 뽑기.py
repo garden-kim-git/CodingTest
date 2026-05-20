232edf5ce553deca494a705d1fea452614f1917b
김정원 <53960185+garden-kim-git@users.noreply.github.com>
2026-05-20 12:35:41 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.55 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,10 +1,6 @@
 def solution(arr, k):
     answer = []
-    # arr = [200, 999, 1, 3, 500, 3, 200] 
-    # mylist = list(set(arr))
     mylist = list(dict.fromkeys(arr))
-    # print(mylist)
-    # mylist.sort()
     
     if len(mylist) > k :
         answer = mylist[:k]
